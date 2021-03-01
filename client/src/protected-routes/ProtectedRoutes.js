a44012937b3f6873c99dfbc95d379ba90d2dc71d
unknown <phyeony@gmail.com>
2021-03-01 17:40:11 +0900

    Troubleshooting protected routing

--- client/src/protected-routes/ProtectedRoutes.js
+++ client/src/protected-routes/ProtectedRoutes.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useUserContext } from '../contexts/UserContext';
 import Error from '../pages/Error';
@@ -8,8 +9,11 @@ export default function ProtectedRoutes({
   ...rest
 }) {
   const { user } = useUserContext();
+  // const [isLoading, setIsLoading] = useState(true);
 
   // I can't use Redirect for the first case bc the it reads states slowly -> Any ways around this?
+  // -> What happens if we don't use await in context? Bad idea?
+  // -> Or "isLoading" useState in UserContext?
   const switchRoute = () => {
     switch (template) {
       case 'accessibleAfterLogin':
@@ -48,5 +52,13 @@ export default function ProtectedRoutes({
         );
     }
   };
+
+  // useEffect(() => {
+  //   if (isLoading) {
+  //     setIsLoading(false);
+  //   }
+  // }, [user]);
+
+  // return <>{isLoading ? '...Loading' : switchRoute()}</>;
   return <>{switchRoute()}</>;
 }

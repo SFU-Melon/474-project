964bc954907c99b49dd2d31018f954ac6e5b5962
cxtruong70 <calvintruong70@gmail.com>
2021-03-10 16:33:28 -0800

    Cleaned up and removed unused imports, states and variables from CreatePost.js and Home's index.js

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,27 +1,14 @@
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useUserContext } from '../../contexts/UserContext';
 import { useEffect, useState, Fragment } from 'react';
 import PostCard from '../../components/PostCard';
-import ImageUpload from '../../components/ImageUpload';
 import CreatePost from './CreatePost';
-import AllUsers from './AllUsers';
 
 export default function Home() {
   const id = 4;
   const { user } = useUserContext();
-  const [content, setContent] = useState('');
 
-  // const [loading, setLoading] = useState(true);
   const [allPosts, setAllPosts] = useState([]);
-
-  const handleSubmit = () => {
-    axios
-      .post(`/api/createPost/${user.id}`, { content: content })
-      .then((res) => {
-        console.log(res);
-      });
-  };
 
   const fetchAllPosts = async () => {
     try {
@@ -39,13 +26,6 @@ export default function Home() {
     fetchAllPosts();
     console.log('useEffect in home');
   }, []);
-
-  const [imgUrl, setImgUrl] = useState(null);
-
-  const uploadCallback = (url) => {
-    console.log(url);
-    setImgUrl(url);
-  };
 
   return (
     <Fragment>

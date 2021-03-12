05a88fd51cbcd084ddd239b4ea31802386040bcf
zsoe <zwemsoe@gmail.com>
2021-03-12 12:20:52 -0800

    post card done

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,29 +1,19 @@
 import Vote from "./Vote";
-import moment from "moment";
 import { useEffect, useState } from "react";
+import Utility from "../utils";
+
 /***
  * props: {
     post,
  * }
  */
 
-const IMG_URL =
-  "https://images.unsplash.com/photo-1506038634487-60a69ae4b7b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1902&q=80";
-
 export default function PostCard({ post }) {
-  const formatTime = () => {
-    //To-Do
-    const now = moment();
-    const post_date = moment(post.datetime);
-    const interval = moment.duration(now.diff(post_date));
-    const minutes = interval.as("minutes");
-    const days = interval.as("days");
-    const hours = interval.as("hours");
-    console.log("hours: " + hours + " days: " + days + " minutes: " + minutes);
-  };
+  const [displayTime, setDisplayTime] = useState("");
 
   useEffect(() => {
-    formatTime();
+    const time = Utility.getDisplayTime(post.datetime);
+    setDisplayTime(time);
   }, []);
 
   return (
@@ -35,7 +25,7 @@ export default function PostCard({ post }) {
       />
       <div>
         <h2>{post.title}</h2>
-        <p>1 hour ago</p>
+        <p>{displayTime}</p>
         <div className="d-flex flex-row justify-content-between">
           <p>
             Posted by {post.authorname} (John) {post.location && "from"}{" "}
---
+++ client/src/utils/index.js
@@ -0,0 +1,23 @@
+import { lightFormat } from "date-fns";
+const Utility = {};
+
+Utility.getDisplayTime = (date) => {
+  const now = new Date();
+  const post_date = new Date(date);
+  const interval = now - post_date;
+  const seconds = Math.round(interval / 1000);
+  if (seconds < 60) {
+    return seconds + " second(s) ago";
+  }
+  const minutes = Math.round(interval / (60 * 1000));
+  if (minutes < 60) {
+    return minutes + " minute(s) ago";
+  }
+  const hours = Math.round(interval / (60 * 60 * 1000));
+  if (hours < 24) {
+    return hours + " hour(s) ago";
+  }
+  return lightFormat(post_date, "dd/MM/yyyy");
+};
+
+export default Utility;

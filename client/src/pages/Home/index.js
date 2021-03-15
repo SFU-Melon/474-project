19e42dac3533be87d0216b76e107e447df743251
zsoe <zwemsoe@gmail.com>
2021-03-15 01:18:14 -0700

    fixed mem leak problem

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,26 +1,33 @@
-import axios from 'axios';
-import { useUserContext } from '../../contexts/UserContext';
-import { useEffect, useState, Fragment } from 'react';
-import PostCard from '../../components/PostCard';
-import CreatePost from './CreatePost';
-import AllUsers from './AllUsers';
+import axios from "axios";
+import { useUserContext } from "../../contexts/UserContext";
+import { useEffect, useState, Fragment } from "react";
+import PostCard from "../../components/PostCard";
+import CreatePost from "./CreatePost";
+import AllUsers from "./AllUsers";
 
 export default function Home() {
   const { user } = useUserContext();
   const [allPosts, setAllPosts] = useState([]);
 
-  const fetchAllPosts = async () => {
+  const fetchAllPosts = async (isMounted) => {
     try {
-      const res = await axios.get('/api/getAllPosts');
-      setAllPosts(res.data);
+      const res = await axios.get("/api/getAllPosts");
+      //setAllPosts(res.data);
+      return res.data;
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    fetchAllPosts();
-    console.log('useEffect in home');
+    let isMounted = true;
+    fetchAllPosts().then((fetched_posts) => {
+      if (isMounted) setAllPosts(fetched_posts);
+    });
+    console.log("useEffect in home");
+    return () => {
+      isMounted = false;
+    };
   }, [user]);
 
   return (

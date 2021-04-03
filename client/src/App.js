08a23ef99c6de74af369dad841dd78ba0a879136
unknown <phyeony@gmail.com>
2021-04-03 14:19:43 +0900

    refactor queries with react-query

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
@@ -12,56 +11,64 @@ import ProtectedRoutes from "./protected-routes/ProtectedRoutes";
 import { AuthProvider } from "@contexts/AuthContext";
 import PublicProfile from "./pages/Profile/PublicProfile";
 import Search from "./pages/Search";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Home from "./pages/Home";
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <AuthProvider>
-      <UserProvider>
-        <Router>
-          <div className="App">
-            <Nav />
-            <div className="content">
-              <Switch>
-                <Route exact path="/post/:title/:id" component={Post} />
-                <Route exact path="/plants" component={Plants} />
-                <Route exact path="/" component={Home} />
-                <Route exact path="/hot" component={Home} />
-                <Route exact path="/new" component={Home} />
-                <Route
-                  exact
-                  path="/profile/public/:username"
-                  component={PublicProfile}
-                />
-                `
-                <ProtectedRoutes
-                  exact
-                  path="/profile/:username"
-                  component={Profile}
-                  template={"accessibleAfterLogin"}
-                />
-                <ProtectedRoutes
-                  exact
-                  path="/login"
-                  component={Login}
-                  template={"accessibleBeforeLogin"}
-                />
-                <ProtectedRoutes
-                  path="/signup"
-                  exact
-                  component={SignUp}
-                  template={"accessibleBeforeLogin"}
-                />
-                <Route path="/search" component={Search} />
-                <Route
-                  path="*"
-                  component={() => <Error msg={"404 NOT FOUND"} />}
-                />
-              </Switch>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <UserProvider>
+          <Router>
+            <div className="App">
+              <Nav />
+              <div className="content">
+                <Switch>
+                  <Route exact path="/post/:title/:id" component={Post} />
+                  <Route exact path="/plants" component={Plants} />
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/hot" component={Home} />
+                  <Route exact path="/new" component={Home} />
+                  <Route
+                    exact
+                    path="/profile/public/:username"
+                    component={PublicProfile}
+                  />
+                  `
+                  <ProtectedRoutes
+                    exact
+                    path="/profile/:username"
+                    component={Profile}
+                    template={"accessibleAfterLogin"}
+                  />
+                  <ProtectedRoutes
+                    exact
+                    path="/login"
+                    component={Login}
+                    template={"accessibleBeforeLogin"}
+                  />
+                  <ProtectedRoutes
+                    path="/signup"
+                    exact
+                    component={SignUp}
+                    template={"accessibleBeforeLogin"}
+                  />
+                  <Route path="/search" component={Search} />
+                  <Route
+                    path="*"
+                    component={() => <Error msg={"404 NOT FOUND"} />}
+                  />
+                </Switch>
+              </div>
             </div>
-          </div>
-        </Router>
-      </UserProvider>
-    </AuthProvider>
+          </Router>
+        </UserProvider>
+      </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 

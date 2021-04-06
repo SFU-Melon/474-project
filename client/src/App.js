b1cf34a77f17107c5a329fb48077b98c29302203
ACimensel <cimensel.a@hotmail.com>
2021-04-06 02:27:41 -0700

    style changes to plants pages

--- client/src/App.js
+++ client/src/App.js
@@ -20,51 +20,6 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-<<<<<<< HEAD
-    <AuthProvider>
-      <UserProvider>
-        <Router>
-          <div className="App">
-            <Nav />
-            <div className="content">
-              <Switch>
-                <Route exact path="/post/:title/:id" component={Post} />
-                <Route exact path="/plants" component={Plants} />
-                <Route exact path="/plants/:sciname" component={Plant} />
-                <Route exact path="/" component={Home} />
-                <Route exact path="/hot" component={Home} />
-                <Route exact path="/new" component={Home} />
-                <Route
-                  exact
-                  path="/profile/public/:username"
-                  component={PublicProfile}
-                />
-                `
-                <ProtectedRoutes
-                  exact
-                  path="/profile/:username"
-                  component={Profile}
-                  template={"accessibleAfterLogin"}
-                />
-                <ProtectedRoutes
-                  exact
-                  path="/login"
-                  component={Login}
-                  template={"accessibleBeforeLogin"}
-                />
-                <ProtectedRoutes
-                  path="/signup"
-                  exact
-                  component={SignUp}
-                  template={"accessibleBeforeLogin"}
-                />
-                <Route path="/search" component={Search} />
-                <Route
-                  path="*"
-                  component={() => <Error msg={"404 NOT FOUND"} />}
-                />
-              </Switch>
-=======
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <UserProvider>
@@ -75,6 +30,7 @@ function App() {
                 <Switch>
                   <Route exact path="/post/:title/:id" component={Post} />
                   <Route exact path="/plants" component={Plants} />
+                  <Route exact path="/plants/:sciname" component={Plant} />
                   <Route exact path="/" component={Home} />
                   <Route exact path="/hot" component={Home} />
                   <Route exact path="/new" component={Home} />
@@ -109,7 +65,6 @@ function App() {
                   />
                 </Switch>
               </div>
->>>>>>> main
             </div>
           </Router>
         </UserProvider>
--- client/src/pages/Plant/index.js
+++ client/src/pages/Plant/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./plantpage.css";
 
 export default function Plant() {
   const [plant, setPlant] = useState();
@@ -17,37 +18,88 @@ export default function Plant() {
     fetchPlant();
   }, []);
   return (
-    <div class="test">
-      <h1>
-        {plant?.comname}
-      </h1>
-      <p>
-        <b>Scientific Name:</b> {plant?.sciname}
-      </p>
-      <p>
-        <b>Description:</b>
-      </p>
-      <p style={{ whiteSpace: 'pre-wrap' }}>
-        {plant?.description}
-      </p>
-      <p>
-        <b>Plant Instruction:</b>
-      </p>
-      <p style={{ whiteSpace: 'pre-wrap' }}>
-        {plant?.plantinstr}
-      </p>
-      <p>
-        <b>Grow Instruction:</b>
-      </p>
-      <p style={{ whiteSpace: 'pre-wrap' }}>
-        {plant?.growinstr}
-      </p>
-      <p>
-        <b>Care Instruction:</b>
-      </p>
-      <p style={{ whiteSpace: 'pre-wrap' }}>
-        {plant?.careinstr}
-      </p>
+    <div class="container">
+      <div class="main-body">
+        <div class="row gutters-sm">
+          <div class="col-md-11">
+            <div class="card mb-3">
+              <div className="card-body">
+
+                <div class="row">
+                  <h1 class="mb-3">
+                    {plant?.comname}
+                  </h1>
+                </div>
+
+                <hr />
+
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">
+                      Scientific Name:
+                    </h6>
+                  </div>
+                  <div class="col-sm-9 text-secondary">
+                    {plant?.sciname}
+                  </div>
+                </div>
+
+                <hr />
+
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">
+                      Description:
+                    </h6>
+                  </div>
+                  <div class="col-sm-9 text-secondary">
+                    {plant?.description}
+                  </div>
+                </div>
+
+                <hr />
+
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">
+                      Plant Instruction:
+                    </h6>
+                  </div>
+                  <div class="col-sm-9 text-secondary">
+                    {plant?.plantinstr}
+                  </div>
+                </div>
+
+                <hr />
+
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">
+                      Grow Instruction:
+                    </h6>
+                  </div>
+                  <div class="col-sm-9 text-secondary">
+                    {plant?.growinstr}
+                  </div>
+                </div>
+
+                <hr />
+
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">
+                      Care Instruction:
+                    </h6>
+                  </div>
+                  <div class="col-sm-9 text-secondary">
+                    {plant?.careinstr}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

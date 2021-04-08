9e2235fe59bcbd6593de08f577cf3581a712d4f7
zsoe <zwemsoe@gmail.com>
2021-04-08 15:47:48 -0700

    created demo s3 bucket

--- db.js
+++ db.js
@@ -1,22 +1,12 @@
+require("dotenv").config();
 const Pool = require("pg").Pool;
 
-let pool;
-if (process.env.NODE_ENV === "production") {
-  pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-  });
-} else {
-  pool = new Pool({
-    user: "postgres",
-    password: "cmpt354",
-    host: "localhost",
-    port: 5432,
-    database: "db354",
-  });
-}
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+});
 
 module.exports = pool;

2d314235bf3359e613264f38fe3208ad4bf4a989
zsoe <zwemsoe@gmail.com>
2021-04-08 15:21:51 -0700

    first commit

--- db.js
+++ db.js
@@ -1,12 +1,21 @@
 const Pool = require("pg").Pool;
 
 // DB Config. Need to be same values
-const pool = new Pool({
-  user: "postgres",
-  password: "cmpt354",
-  host: "localhost",
-  port: 5432,
-  database: "db354",
-});
+if (process.env.NODE_ENV === "production") {
+  const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: 5432,
+  });
+} else {
+  const pool = new Pool({
+    user: "postgres",
+    password: "cmpt354",
+    host: "localhost",
+    port: 5432,
+    database: "db354",
+  });
+}
 
 module.exports = pool;

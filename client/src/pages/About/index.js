5f23281b6cd73e344ad38009185b84493ab40644
unknown <phyeony@gmail.com>
2021-04-11 18:56:26 +0900

    add about in nav. change about page ul name

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -6,11 +6,11 @@ import useLocalStorage from "@hooks/useLocalStorage";
 import ScreenLoading from "@components/ScreenLoading";
 import "./style.css";
 
-
 const About = () => {
   return (
     <Fragment>
-      <div className="full-plant-bg"
+      <div
+        className="full-plant-bg"
         style={{
           backgroundImage: `url(${process.env.PUBLIC_URL + "/plant-bg.png"})`,
         }}
@@ -21,37 +21,53 @@ const About = () => {
           </div>
 
           <div className="d-flex flex-row flex-wrap justify-content-center">
-            <div className = "card about-paragraph p-3 m-1">
-                <h3>Introduction</h3>
-                <p>Planter's Social is a platform for plant enthusiasts. Its main functionality is to
-                allow users to share posts with descriptions, and images of their plants so that
-                other users can then like, save and their comments. </p>
+            <div className="card about-paragraph p-3 m-1">
+              <h3>Introduction</h3>
+              <p>
+                Planter's Social is a platform for plant enthusiasts. Its main
+                functionality is to allow users to share posts with
+                descriptions, and images of their plants so that other users can
+                then like, save and their comments.{" "}
+              </p>
             </div>
-            <div className = "card about-paragraph p-3 m-1">
-                <h3>Purpose</h3>
-                <p>Over time users can like and save
-                posts for inspiration, share fruits of their own labor, and build a community fostering
-                appreciation for the growing of plants.</p>
+            <div className="card about-paragraph p-3 m-1">
+              <h3>Purpose</h3>
+              <p>
+                Over time users can like and save posts for inspiration, share
+                fruits of their own labor, and build a community fostering
+                appreciation for the growing of plants.
+              </p>
             </div>
-            <div className = "card about-paragraph p-3 m-1">
-                <h3>Technology Stack</h3>
-                <code>Front-end</code>
-                <p>We used a combination of React, various react packages
-                  and Bootstrap for our front-end. </p>
-                  <code>Back-end</code>
-                  <p>For our back-end we used Node.js and Express.js
-                  alongside PostgreSQL for our database. 
-                </p>
+            <div className="card about-paragraph p-3 m-1">
+              <h3>Technology Stack</h3>
+              <code>Front-end</code>
+              <p>
+                We used a combination of React, various react packages and
+                Bootstrap for our front-end.{" "}
+              </p>
+              <code>Back-end</code>
+              <p>
+                For our back-end we used Node.js and Express.js alongside
+                PostgreSQL for our database.
+              </p>
             </div>
-            <div className = "card about-paragraph p-3 m-1">
+            <div className="card about-paragraph p-3 m-1">
               <h3>Creators</h3>
-                <p>Planters Social is brought to you by:</p>
-                <ul>
-                  <li><a href="https://github.com/phyeony">Hyeonyoung Park</a></li>
-                  <li><a href="https://github.com/cxtruong70">Calvin Truong</a></li>
-                  <li><a href="https://github.com/zwemsoe">Zwe Min Soe</a></li>
-                  <li><a href="https://github.com/ACimensel">Artun Cimensel</a></li>
-                </ul>
+              <p>Planters Social is brought to you by:</p>
+              <ul className="creator-list">
+                <li>
+                  <a href="https://github.com/phyeony">Hyeonyoung Park</a>
+                </li>
+                <li>
+                  <a href="https://github.com/cxtruong70">Calvin Truong</a>
+                </li>
+                <li>
+                  <a href="https://github.com/zwemsoe">Zwe Min Soe</a>
+                </li>
+                <li>
+                  <a href="https://github.com/ACimensel">Artun Cimensel</a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>

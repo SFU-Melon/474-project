c587a2d815b70e1b4c107e9a1987857f4dfb095f
phyeony <hyeonyoung_park@sfu.ca>
2023-03-10 23:02:21 -0800

    Add amplify.yml forreact hosting. Troubleshoot amplify deployment

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: `${process.env.REACT_APP_API_GATEWAY_URL}`,
       changeOrigin: true,
     })
   );

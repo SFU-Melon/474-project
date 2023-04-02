5510eccec861d0d610832f4ae16eb5130f4e1376
vy-b <kvy.bui@gmail.com>
2023-04-02 16:50:47 -0700

    change votes.py

--- lambda/comments.py
+++ lambda/comments.py
@@ -1,147 +1,94 @@
+from datetime import datetime
+from uuid import uuid4
 import json
+# import the AWS SDK (for Python the package name is boto3)
 import boto3
-
-def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
+# have to import this namespace individually
+from boto3.dynamodb.conditions import Attr
+    
+def lambda_handler( event, context ):
+    """Setup DB connection"""
     client = boto3.client('dynamodb')
-    body = json.loads(event['body'])
-    username = event["pathParameters"]["username"]
-    endpoint = event["path"]
-    res = None
-    # get the current post to make sure it exists
-    if (body['type'] == 'post'):
-        typeTable = 'posts'
-        likesTable = 'likes'
-        likesQueryKey = {'postid': {
-                      'S': body['votedId']
-                    },
-                    'userid':{
-                        'S':username
-                    }
-                    }
-        queryId = {
-                'id': {
-                  'S': body['votedId']
-                }
-            }
-        
-    elif (body['type'] == 'comment'):
-        typeTable = 'comments'
-        likesTable = 'likescomment'
-        likesQueryKey = {'commentid': {
-                      'S': body['votedId']['commentId']
-                    },
-                    'userid':{
-                        'S':username
-                    }
-                    }
-        queryId = {
-                'id': {
-                  'S': body['votedId']['commentId']
-                }
-            }
-    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('comments')
+    # use the DynamoDB object to select our table
+    ENDPOINT = event["path"]
+    PATHPARAMS = event["pathParameters"]
+    res = {}
+    """Handle Requests"""
     try:
-        data = client.get_item(
-            TableName=typeTable,
-            Key=queryId
-          )
-        postExists = data['Item']
-        if (not postExists):
-            raise
-        
-        # if post does exist, check if user has liked it yet
-        likeData = client.get_item(
-            TableName=likesTable,
-            Key=likesQueryKey
-          )
-       
-    except Exception as e:
-        # condition check prob failed
-        return {
-            'statusCode': 500,
-            'body': json.dumps({"get post/comments and likes failed": str(e)})
-        }
-        
-    if ('Item' in likeData):
-        likeStatus = likeData['Item']["val"]["N"]
-        numLikes = int(postExists["numoflikes"]["N"])
-        
-        # handle upvote
-        if ("upVotePost" in endpoint):
-            # if it is currently liked aka val = 1, action will cancel the vote/like
-            if (likeStatus=="1"):
-                likeStatus = 0
-                numLikes -= 1
-                res = "cancelled upvote on post"
-            # if currently downvoted, action will change to upvoted and add 2 likes
-            elif (likeStatus == "-1"):
-                likeStatus = 1
-                numLikes += 2
-                res = "updated to upvote from downvote"
-            # if it is currently not liked aka val = 0, then it will update to liked (val = 1)
-            else: # likeStatus = 0 indicating not liked yet
-                numLikes += 1
-                likeStatus = 1
-                res = "upvoted"
-                
-        # handle downvote
-        elif ("downVotePost" in endpoint):
-            if (likeStatus == "-1"):
-                likeStatus = 0
-                numLikes += 1
-                res = "cancelled downvote on post"
-            elif (likeStatus == "1"):
-                likeStatus = -1
-                numLikes -= 2
-                res = "updated to downvote from upvote"
-            # if it is currently not downvoted aka val = 0, then it will update to downvoted (val = 1)
-            else: # likeStatus = 0 indicating not liked yet
-                numLikes -= 1
-                likeStatus = -1
-                res = "downvoted"
-        
-        # update the user's like status in the like table to the correct value
-        client.update_item(
-            TableName=likesTable,
-            Key=likesQueryKey,
-            UpdateExpression="set val=:l",
-            ExpressionAttributeValues={
-                ':l': {"N":str(likeStatus)}},
-            ReturnValues="UPDATED_NEW"
-        )
-       
-    # if user has never voted on this before, must insert new item into likes table
-    else:
-        new_item = likesQueryKey
-        new_item['val'] = {"N": "1"}
-        numLikes = int(postExists["numoflikes"]["N"]) + 1
-        client.put_item(
-            TableName= likesTable,
-            Item= new_item,
-            ConditionExpression= "attribute_not_exists(userid)"
-        )
-   
-    
-    # finally, update number of likes in posts table accordingly.
-    try:
-        response = client.update_item(
-            TableName=typeTable,
-            Key=queryId,
-            UpdateExpression="set numoflikes=:l",
-            ExpressionAttributeValues={
-                ':l': {"N":str(numLikes)}},
-            ReturnValues="UPDATED_NEW")
+        if "getComments" in ENDPOINT:
             
-    except Exception as err:
+            data = table.scan(
+                FilterExpression=Attr('postid').eq(PATHPARAMS['postid'])
+            )
+            parsed_data = [{"id": item["id"],"content": item["content"],"postid":item["postid"],"datetime":item["datetime"],
+                "username":item["userid"],"numoflikes":str(item["numoflikes"])
+            } for item in data['Items']]
+            
+            key = 'numoflikes'
+            parsed_data.sort( key=lambda item: item[key], reverse=True )
+            res['comments']=parsed_data
+    
+        elif "submitComment" in ENDPOINT:
+            data = json.loads(event['body'])
+            userid = PATHPARAMS['userid']
+            postid = PATHPARAMS['postid']
+            u_id = uuid4()
+            timestamp = datetime.today().strftime('%Y-%m-%d') # casts to .0 instead of ugly .23740
+    
+            transaction = [
+                {
+                    # this should
+                    "Put": {
+                    "TableName": "comments",
+                    "Item": {
+                        "id": {"S": str(u_id)},
+                        "datetime": {"S": timestamp},
+                        "numoflikes": {"N": '0'},
+                        "userid": {"S": userid},
+                        "postid": {"S":postid},
+                        "content": {"S": data['content']},
+                        },
+                    "ConditionExpression": "attribute_not_exists(id)"
+                    },
+                }
+            ]
+    
+            try:
+                client.transact_write_items(
+                    TransactItems = transaction
+                )
+            except Exception as e:
+                # condition check prob failed
+                return {
+                    'statusCode': 500,
+                    'body': json.dumps({"error": str(e)})
+                }
+            # transact_write_items doesnt return item so just append
+            # some fields to the input dict and return it instead
+            comment = data
+            comment["username"] = userid
+            comment["id"] = str(u_id)
+            comment["datetime"] = timestamp
+            comment["numoflikes"] = 0
+            res["success"] = 1
+            res["comment"] = comment
+
+
+    except Exception as e: # invalid request
+        # raise Exception("Invalid request")
+        res["success"] = 0
         return {
-        'statusCode': 500,
-        'body': json.dumps({"error": str(err)})
+                'statusCode': 400,
+                'body': json.dumps({"error": str(e)})
         }
-    
-    
+
+    # assume all went well
     return {
         "statusCode": 200,
         'headers': {'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'},
-        "body": res
+        "body": json.dumps(res)
     }
+
+
--- lambda/vote.py
+++ lambda/vote.py
@@ -2,20 +2,49 @@ import json
 import boto3
 
 def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
     client = boto3.client('dynamodb')
     body = json.loads(event['body'])
     username = event["pathParameters"]["username"]
     endpoint = event["path"]
     res = None
     # get the current post to make sure it exists
-    try:
-        data = client.get_item(
-            TableName='posts',
-            Key={
+    if (body['type'] == 'post'):
+        typeTable = 'posts'
+        likesTable = 'likes'
+        likesQueryKey = {'postid': {
+                      'S': body['votedId']
+                    },
+                    'userid':{
+                        'S':username
+                    }
+                    }
+        queryId = {
                 'id': {
                   'S': body['votedId']
                 }
             }
+        
+    elif (body['type'] == 'comment'):
+        typeTable = 'comments'
+        likesTable = 'likescomment'
+        likesQueryKey = {'commentid': {
+                      'S': body['votedId']['commentId']
+                    },
+                    'userid':{
+                        'S':username
+                    }
+                    }
+        queryId = {
+                'id': {
+                  'S': body['votedId']['commentId']
+                }
+            }
+    
+    try:
+        data = client.get_item(
+            TableName=typeTable,
+            Key=queryId
           )
         postExists = data['Item']
         if (not postExists):
@@ -23,22 +52,17 @@ def lambda_handler(event, context):
         
         # if post does exist, check if user has liked it yet
         likeData = client.get_item(
-            TableName='likes',
-            Key={
-                'postid': {
-                  'S': body['votedId']
-                },
-                'userid':{
-                    'S':username
-                }
-            }
+            TableName=likesTable,
+            Key=likesQueryKey
           )
+       
     except Exception as e:
         # condition check prob failed
         return {
             'statusCode': 500,
-            'body': json.dumps({"big err": str(e)})
+            'body': json.dumps({"get post/comments and likes failed": str(e)})
         }
+        
     if ('Item' in likeData):
         likeStatus = likeData['Item']["val"]["N"]
         numLikes = int(postExists["numoflikes"]["N"])
@@ -76,29 +100,25 @@ def lambda_handler(event, context):
                 numLikes -= 1
                 likeStatus = -1
                 res = "downvoted"
-            
         
         # update the user's like status in the like table to the correct value
         client.update_item(
-            TableName='likes',
-            Key={'userid': {
-                'S': username
-            },'postid':{'S':body["votedId"]}},
+            TableName=likesTable,
+            Key=likesQueryKey,
             UpdateExpression="set val=:l",
             ExpressionAttributeValues={
                 ':l': {"N":str(likeStatus)}},
             ReturnValues="UPDATED_NEW"
         )
+       
     # if user has never voted on this before, must insert new item into likes table
     else:
+        new_item = likesQueryKey
+        new_item['val'] = {"N": "1"}
         numLikes = int(postExists["numoflikes"]["N"]) + 1
         client.put_item(
-            TableName= 'likes',
-            Item= {
-                "userid": {"S": username},
-                "postid": {"S": body['votedId']},
-                "val":{"N": "1"}
-                },
+            TableName= likesTable,
+            Item= new_item,
             ConditionExpression= "attribute_not_exists(userid)"
         )
    
@@ -106,10 +126,8 @@ def lambda_handler(event, context):
     # finally, update number of likes in posts table accordingly.
     try:
         response = client.update_item(
-            TableName='posts',
-            Key={'id': {
-                'S': body['votedId']
-            }},
+            TableName=typeTable,
+            Key=queryId,
             UpdateExpression="set numoflikes=:l",
             ExpressionAttributeValues={
                 ':l': {"N":str(numLikes)}},
@@ -120,7 +138,6 @@ def lambda_handler(event, context):
         'statusCode': 500,
         'body': json.dumps({"error": str(err)})
         }
-    
     
     
     return {
